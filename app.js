7e824fbe59b57afb7ab3fe19fcd1eccf5f3144ee
unknown <lap.hoang.idi@gmail.com>
2021-03-30 16:08:26 +0200

    download added

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const printer = require("pdf-to-printer");
 const express = require("express");
+const download = require("./config/downloadPDF");
 const randomId = require("random-id");
 const logger = require("./config/winston");
-const timeConverter = require('./config/timeConverter');
+const timeConverter = require("./config/timeConverter");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
@@ -10,10 +11,12 @@ const PORT = 3000;
 const LENGTH = 7;
 const pattern = "aA0";
 
+// ============= MIDDLEWARES===================
 app.use(express.json());
 app.use(cors());
 app.use(morgan("combined", { stream: logger.stream }));
 
+// ============END POINTS======================
 app.post("/print", (req, res) => {
   let printSettings, options, jobID;
 
@@ -32,35 +35,61 @@ app.post("/print", (req, res) => {
     win32: [printSettings],
   };
 
-  // check if requested printer is available
-  printer.getPrinters()
-  .then(allPrinters => {
-    if(allPrinters.includes(printerID)) {
+  download(path, fileName)
+    .then((success) => {
+      let file = "pdf/" + fileName;
       printer
-      .print(path + fileName, options)
-      .then(response => {
-        logger.info(`${req.ip} - - ${timeConverter(Date.now())} - successful print request with ID: ${jobID} | fileName: ${fileName}`);
-        res.send(`success, ${fileName} is being printed by ${printerID} with ID: ${jobID}`)
-      })
-      .catch(error => {
-        logger.error(`${req.ip} - - ${timeConverter(Date.now())} - print request failed with ID: ${jobID} || see => ${error.cmd}`)
-        res.send(`print request failed with ID: ${jobID} - please see logs.`)
-      });
-    } else {
-      res.send(`no printer matches ${printerID}, please use another one or try again!`)
-      return;
-    }
-  })
-  .catch(err => {
-    console.log(err)
-  })
+        .getPrinters()
+        .then((allPrinters) => {
+          if (allPrinters.includes(printerID)) {
+            printer
+              .print(file, options)
+              .then((response) => {
+                logger.info(
+                  `${req.ip} - - ${timeConverter(
+                    Date.now()
+                  )} - successful print request with ID: ${jobID} | fileName: ${fileName}`
+                );
+                res.send(
+                  `success, ${fileName} is being printed by ${printerID} with ID: ${jobID}`
+                );
+              })
+              .catch((error) => {
+                logger.error(
+                  `${req.ip} - - ${timeConverter(
+                    Date.now()
+                  )} - print request failed with ID: ${jobID} || see => ${
+                    error.cmd
+                  }`
+                );
+                res.send(
+                  `print request failed with ID: ${jobID} - please see logs.`
+                );
+              });
+          } else {
+            res.send(
+              `no printer matches ${printerID}, please use another one or try again!`
+            );
+            return;
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((error) => console.error(error));
+
+  // check if requested printer is available
 });
 
 app.get("/test", (req, res) => {
-  printer.getDefaultPrinter()
-  .then(defaultPrinter => {console.log(defaultPrinter)})
-  .catch(error => console.error(error));
-  
+  printer
+    .getDefaultPrinter()
+    .then((defaultPrinter) => {
+      console.log(defaultPrinter);
+    })
+    .catch((error) => console.error(error));
+
   res.send("Working good, See the logs in console for printers");
 });
 
